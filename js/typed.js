6809dc571aeba9c9c6cc3e9d3d4a8307ee9d1e25
gajdikuka <gajdikuka@hotmail.com>
2022-05-02 21:21:58 +0200

    cause removed

--- js/typed.js
+++ js/typed.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#typed', {
-    strings: ['voice', 'talent', 'business', 'dream', 'cause'],
+    strings: ['voice', 'talent', 'business', 'dream'],
     typeSpeed: 30,
     backSpeed: 30,
     backDelay: 1500,
