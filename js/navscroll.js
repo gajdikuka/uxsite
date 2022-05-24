efd43d7b1768928e18d9f8a8d8a0fcac66f0ab91
gajdikuka <gajdikuka@hotmail.com>
2022-05-24 17:19:39 +0200

    navscroll fix attempt

--- js/navscroll.js
+++ js/navscroll.js
@@ -1,16 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-
     el_autohide = document.querySelector('.autohide');
-
-    // add padding-top to bady (if necessary)
     navbar_height = document.querySelector('.navbar').offsetHeight;
     document.body.style.paddingTop = navbar_height + 'px';
-
     if (el_autohide) {
         var last_scroll_top = 0;
         window.addEventListener('scroll', function() {
             let scroll_top = window.scrollY;
-            if (scroll_top < last_scroll_top) {
+            if (scroll_top <= last_scroll_top || scroll_top <= navbar_height * 3) {
                 el_autohide.classList.remove('scrolled-down');
                 el_autohide.classList.add('scrolled-up');
             } else {
@@ -19,9 +15,5 @@ document.addEventListener("DOMContentLoaded", function() {
             }
             last_scroll_top = scroll_top;
         });
-        // window.addEventListener
     }
-    // if
-
 });
-// DOMContentLoaded  end
